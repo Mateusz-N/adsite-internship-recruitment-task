f36ea9b72b2d6d1efd19a3d3fd7bd69a04ea86b4
Mateusz-N <nosel.mateusz@gmail.com>
2024-09-04 20:16:32 +0200

    Add home page component, clean up the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import "./App.css";
-const logo = require("./logo.svg");
+import HomePage from "./pages/Home"
 
-const App = () => {
+import "./App.css";
+
+const App: React.FC<{}> = () => {
   return (
-    <div className = "App">
-      <header className = "App-header">
-        <img src = {logo} className = "App-logo" alt = "logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className = "App-link"
-          href = "https://reactjs.org"
-          target = "_blank"
-          rel = "noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomePage/>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,30 @@
+import { Fragment } from 'react';
+
+const HomePage: React.FC<{}> = () => {
+    return(
+        <Fragment>
+            <nav>
+
+            </nav>
+            <main>
+
+            </main>
+            <section>
+                <header>
+
+                </header>
+                <main>
+
+                </main>
+            </section>
+            <aside>
+
+            </aside>
+            <footer>
+
+            </footer>
+        </Fragment>
+    );
+}
+
+export default HomePage;
